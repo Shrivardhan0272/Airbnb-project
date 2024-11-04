ae5a780c4c8d488337b12c912716849c2810c9c4
Shrivardhan0272 <shrivardhanr1@gmail.com>
2024-11-04 18:49:44 +0530

    This is the code

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.post("/listings",async(req,res)=>{
 app.put("/listings/:id",async(req,res)=>{
     let {id}=req.params;
    await  Listing.findByIdAndUpdate(id,{...req.body.listing});
-    console.log({...req.body.listing});
+   
    res.redirect(`/listings/${id}`);
 
 })
@@ -85,6 +85,6 @@ app.get("/listings/:id/edit",async(req,res)=>{
 app.delete("/listings/:id",async(req,res)=>{
     let {id}=req.params;
     let deleted= await Listing.findByIdAndDelete(id);
-    console.log(deleted);
+   
     res.redirect("/listings");
 })
